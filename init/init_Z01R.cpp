66bdcde3b1ff832425e1d73b56f1e752604add2c
rohan <purohit.rohan@gmail.com>
2019-12-12 21:14:58 +0530

    Z01R: set ro.product to ASUS_Z01R_1
    * for camera pp

--- init/init_Z01R.cpp
+++ init/init_Z01R.cpp
@@ -56,6 +56,7 @@ void property_override_dual(char const system_prop[], char const vendor_prop[],
 void vendor_load_properties()
 {
     // fingerprint
+    property_override("ro.product.device", "ASUS_Z01R_1");
     property_override("ro.build.description", "Z01R-user 9 PPR1.180610.009 WW_user_90.11.162.88_20190723 release-keys");
     property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
 }
