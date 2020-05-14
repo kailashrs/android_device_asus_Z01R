b4556f1748adbdea32e02482d26b1f3b36c72e8d
Kailash <kailash.sudhakar@gmail.com>
2020-05-15 04:49:48 +0530

    Revert "Z01R: use custom doze"
    This reverts commit 17c206108e558698547ff0da1d1ee612bf450f5e.

--- device.mk
+++ device.mk
@@ -98,13 +98,9 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.0
 
 # Doze
-#PRODUCT_PACKAGES += \
-#    AsusDoze \
-#    AsusPocketMode
-
-# Doze
 PRODUCT_PACKAGES += \
-    CustomDoze
+    AsusDoze \
+    AsusPocketMode
 
 # FM
 PRODUCT_PACKAGES += \
