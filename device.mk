a6d940be1e07a96dd9fe4f8f84fc236c8fa86965
Kailash <kailash.sudhakar@gmail.com>
2020-08-06 18:39:46 +0530

    Z01R: Add manifest to vendor_overlay
    To prevent vendor.qti.hardware.cacert@1.0 from starting. Hacky, but I don't have a choice.
    
    Signed-off-by: Kailash <kailash.sudhakar@gmail.com>

--- device.mk
+++ device.mk
@@ -66,7 +66,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:system/etc/powerhint.json \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/msm_irqbalance.conf \
     $(LOCAL_PATH)/configs/android.hardware.graphics.composer@2.3-service.rc:system/product/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init/android.hardware.graphics.composer@2.3-service.rc \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/fstab.qcom
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/fstab.qcom \
+    $(LOCAL_PATH)/configs/manifest.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/vintf/manifest.xml
 
 # Boot control
 PRODUCT_PACKAGES_DEBUG += \
